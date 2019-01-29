e7f736726256fc9a8f9b983525add2adaf3c2424
Brendan Carney <bcarney02@gmail.com>
2019-01-29 07:40:01 -0600

    Add commonjs to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import pkg from "./package.json";
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default [
   {
     external: ["react", "react-dom", "immutable", "slate", "slate-react"],
-    plugins: [babel(), resolve()],
+    plugins: [babel(), resolve(), commonjs()],
     output: [{ file: pkg.module, format: "es" }]
   }
 ];
