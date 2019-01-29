b0aef28f2a228fb83496baac98768a99cf69b83d
Brendan Carney <bcarney02@gmail.com>
2019-01-29 07:49:05 -0600

    Update docz site description

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,8 @@ import { css } from "docz-plugin-css";
 
 export default {
   title: "ConvertKit Slate Plugins",
+  description:
+    "A collection of plugins for SlateJS maintained by the ConvertKit team.",
   menu: ["Development"],
   plugins: [
     css({
