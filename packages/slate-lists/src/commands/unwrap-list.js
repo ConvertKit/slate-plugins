d6f3c25084f2bd8ebcb52e357b8b62fded08db32
alexej-d <alexej-d@users.noreply.github.com>
2019-06-04 15:47:14 +0200

    Fix deprecation warning
    As of slate@0.47, the `getNodesAtRange` method has been renamed to `getDescendantsAtRange`.

--- packages/slate-lists/src/commands/unwrap-list.js
+++ packages/slate-lists/src/commands/unwrap-list.js
@@ -2,7 +2,7 @@ import unwrapListByKey from "./unwrap-list-by-key";
 
 export default ({ blocks }, editor) => {
   const listItemChildren = editor.value.document
-    .getNodesAtRange(editor.value.selection)
+    .getDescendantsAtRange(editor.value.selection)
     .filter(node => node.type == blocks.list_item_child);
 
   const furthestListItems = listItemChildren
